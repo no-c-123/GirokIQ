4fa468f0f7eaebfaa4317c9d90cdf6b77edb01fa
Hector Leal <136043873+no-c-123@users.noreply.github.com>
2026-01-24 00:01:40 -0600

    chore: initial project setup with React, TS, Tailwind

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen bg-neutral-900 text-neutral-100 flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        App initialized
+      </h1>
+    </div>
+  );
+}
